0dcd8f0362251f659219d68d35759492aa21e971
Ashley Blewer <ashley.blewer@gmail.com>
2021-07-05 16:28:15 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// I know this is like, annoying, but it seems ok
+// as a means of breaking these into grabbable components
+// in the future?
+
+import DailyCallContainer from "./js/daily-call-container.js";
+import DailyCallWindow from "./js/daily-call-window.js"
+import DailyTray from "./js/daily-tray.js"
+import DailyJoinCall from "./js/daily-call-join.js"
+import DailyLeaveCall from "./js/daily-call-leave.js"
+import DailyToggleAudio from "./js/daily-toggle-audio.js"
+import DailyToggleVideo from "./js/daily-toggle-video.js"
+import DailyToggleScreen from "./js/daily-toggle-screen.js"
+
+export {
+    DailyCallContainer,
+    DailyCallWindow,
+    DailyTray,
+    DailyJoinCall,
+    DailyLeaveCall,
+    DailyToggleAudio,
+    DailyToggleVideo,
+    DailyToggleScreen
+}
